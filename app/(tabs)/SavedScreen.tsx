5d7d786203c1c242b180bfd06a5d2610208061f0
Bobby Sandhu <BobbySandhu@users.noreply.github.com>
2024-10-06 15:39:19 +0530

    added tabs and screens

---
+++ app/(tabs)/SavedScreen.tsx
@@ -0,0 +1,7 @@
+import { View, Text } from "react-native"
+
+export default function SavedScreen() {
+    return <View>
+        <Text>Saved Screen</Text>
+    </View>
+}
---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,45 @@
+import { TabBarIcon } from '@/components/navigation/TabBarIcon';
+import { Tabs } from 'expo-router';
+
+export default function TabLayout() {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Jokes',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon
+              name={focused ? 'home' : 'home-outline'}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="SearchScreen"
+        options={{
+          title: 'Search',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon
+              name={focused ? 'search' : 'search-outline'}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="SavedScreen"
+        options={{
+          title: 'Saved',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon
+              name={focused ? 'heart' : 'heart-outline'}
+              color={color}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
