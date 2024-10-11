6e6c92a8455e4237329442d66f285734512351d3
Bobby Sandhu <BobbySandhu@users.noreply.github.com>
2024-10-11 15:28:12 +0530

    added api for search screen

--- app/(tabs)/SearchScreen.tsx
+++ app/(tabs)/SearchScreen.tsx
@@ -1,10 +1,46 @@
-import { View, Text } from 'react-native';
+import { View, Text, ActivityIndicator, FlatList } from 'react-native';
 import { style } from '../../common/appStyles';
+import CategoryCardView from '@/components/JokeCategory';
+import { useEffect, useState } from 'react';
 
 export default function SearchScreen() {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const fetchCategories = async () => {
+    try {
+      const response = await fetch(
+        'https://official-joke-api.appspot.com/types'
+      );
+      const json = await response.json();
+      setData(json);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
   return (
     <View style={style.rootContainer}>
-      <Text>Search Screen</Text>
+      {isLoading ? (
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <ActivityIndicator />
+        </View>
+      ) : (
+        <FlatList
+          data={data}
+          //keyExtractor={({ id }) => id}
+          renderItem={({ item }) => <CategoryCardView type={item} />}
+          showsVerticalScrollIndicator={false}
+          ItemSeparatorComponent={() => <View style={{ height: 20 }}></View>}
+          contentContainerStyle={{ paddingVertical: 16, paddingHorizontal: 16 }}
+        />
+      )}
     </View>
   );
 }
---
+++ components/JokeCategory.tsx
@@ -0,0 +1,29 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function CategoryCardView(props) {
+  return (
+    <View style={styles.rootContainer}>
+      <Text style={styles.text}>
+        {props.type.charAt(0).toUpperCase() + props.type.slice(1)}
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    padding: 18,
+    backgroundColor: '#ffffff',
+    shadowColor: 'black',
+    shadowOpacity: 0.3,
+    shadowRadius: 5,
+    shadowOffset: { height: 0, width: 1 },
+    borderRadius: 8,
+    elevation: 8,
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 17,
+    fontWeight: 'bold',
+  },
+});
