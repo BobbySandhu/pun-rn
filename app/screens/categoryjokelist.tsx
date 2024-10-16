242c665f350c6e3c0c15b407a94ce9aebe6b0ef7
Bobby Sandhu <BobbySandhu@users.noreply.github.com>
2024-10-16 13:29:02 +0530

    added - added category jokes screen

--- app/screens/categoryjokelist.tsx
+++ app/screens/categoryjokelist.tsx
@@ -1,9 +1,51 @@
-import { View, Text } from 'react-native';
+import { style } from '@/common/appStyles';
+import JokeCardView from '@/components/JokeCardView';
+import { useLocalSearchParams } from 'expo-router';
+import { useEffect, useState } from 'react';
+import { View, Text, ActivityIndicator, FlatList } from 'react-native';
 
 export default function CategoryJokeList() {
+  const local = useLocalSearchParams();
+
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const fetchCategoryJokes = async () => {
+    try {
+      const response = await fetch(
+        `https://official-joke-api.appspot.com/jokes/${local.type}/ten`
+      );
+      const json = await response.json();
+      setData(json);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchCategoryJokes();
+  }, []);
+
   return (
-    <View>
-      <Text>Category Jokes...</Text>
+    <View style={style.rootContainer}>
+      {isLoading ? (
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <ActivityIndicator />
+        </View>
+      ) : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }) => id}
+          renderItem={({ item }) => (
+            <JokeCardView setup={item.setup} punchline={item.punchline} />
+          )}
+          showsVerticalScrollIndicator={false}
+          ItemSeparatorComponent={() => <View style={{ height: 20 }} />}
+          contentContainerStyle={{ paddingVertical: 16, paddingHorizontal: 16 }}
+        />
+      )}
     </View>
   );
 }
